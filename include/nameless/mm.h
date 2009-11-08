ebac5eac4f336ea60265ea33210c87e5e1ccaa50
Yoshifumi Shimono <yoshifumi.shimono@gmail.com>
2009-11-08 16:49:35 +0900

    Licensing: Add GPL copyright notices.

--- include/nameless/mm.h
+++ include/nameless/mm.h
@@ -1,6 +1,24 @@
 #ifndef _NAMELESS_MM_H_
 #define _NAMELESS_MM_H_
 
+/*
+ * Nameless - A lambda calculation language.
+ * Copyright (C) 2009 Yoshifumi Shimono <yoshifumi.shimono@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include <stdlib.h>
 #include <stdint.h>
 #include "nameless.h"
